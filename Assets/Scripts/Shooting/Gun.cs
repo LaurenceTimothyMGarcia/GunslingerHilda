f10e8ba5c2baf6f483821c5d2dbb63fcb4cfaa06
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-09-24 23:01:09 -0700

    started this, got lazy, we aint doing this no mo

---
+++ Assets/Scripts/Shooting/Gun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Bullet
+    public GameObject bullet;
+
+    // Bulet force
+    public float shootForce;
+    public float upwardForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
