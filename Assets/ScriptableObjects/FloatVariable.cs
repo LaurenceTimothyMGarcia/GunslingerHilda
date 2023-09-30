882913fd602e693651aae255b78373ebb200a7a8
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-09-30 08:49:13 -0700

    testing out scripable objects

---
+++ Assets/ScriptableObjects/FloatVariable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Scriptable object that can be used to store data and variables for the project without being dependent
+
+[CreateAssetMenu]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+}
