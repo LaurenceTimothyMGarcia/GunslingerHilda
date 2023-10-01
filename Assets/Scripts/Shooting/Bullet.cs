432d24cb17ed1fbae08ba9672d9167bc3972e8bc
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-10-01 07:17:32 -0700

    added bullet lifetime so nothing goes forever

--- Assets/Scripts/Shooting/Bullet.cs
+++ Assets/Scripts/Shooting/Bullet.cs
@@ -6,6 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public BulletPowerProps activePowers;
 
+    public float bulletLifetime = 5f;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, bulletLifetime);
+    }
+
     public void OnCollisionEnter(Collision col)
     {
 
@@ -44,40 +51,58 @@ public class Bullet : MonoBehaviour
         {
             ExplosiveBullets();
         }
+
+        // On Collide
+        if (!col.gameObject.CompareTag("Player"))
+        {
+            if (col.gameObject.CompareTag("Enemy"))
+            {
+                // Deal Damage to enemy
+            }
+
+            Destroy(this.gameObject);
+        }
     }
 
     public void FireBullets()
     {
         // Insert code here
+        Debug.Log("Fire Bullet");
     }
 
     public void HomingBullets()
     {
         // Insert code here
+        Debug.Log("Homing");
     }
 
     public void PiercingShots()
     {
         // Insert code here
+        Debug.Log("Pierce");
     }
 
     public void StunBullets()
     {
         // Insert code here
+        Debug.Log("Stun");
     }
 
     public void SlowBullets()
     {
         // Insert code here
+        Debug.Log("Slow");
     }
 
     public void KnockbackBullets()
     {
         // Insert code here
+        Debug.Log("Knockback");
     }
 
     public void ExplosiveBullets()
     {
         // Insert code here
+        Debug.Log("Explosive");
     }
 }
