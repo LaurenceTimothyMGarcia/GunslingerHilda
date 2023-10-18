b930911fb31196fd328a7c0cd42acd383c1cd8da
Dachios <145947955+Dachios@users.noreply.github.com>
2023-10-18 15:34:30 -0700

    DummyEnemy Script and Health Bar Functionality
    Whipped up a quick script to work with changing the healthbar, also majority ripped from the Easy HealthBar video. In addition, I went into TempBullet and enabled its Sphere Collider and checked the Trigger Box. Currently, it's detected by the name of the bullet prefab (In this case, it happens to be TempBullet(Clone) ), so if anything else hits it, it doesn't take damage.
    
    P.S. - Larry please make the bullet script destroy the bullet clone after a certain range.
    P.P.S - DummyHealth and EnemyHealth are confusing nameschemes. They're in different folders but maybe I should do something about it.

---
+++ Assets/Scripts/Enemy/DummyHealth/DummyHealth.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Cobbled together from seeing some things from the Healthbar tutorial and other things and intuition.
+// Learned that SerializeField makes it available AND editable in Unity!
+public class DummyHealth : MonoBehaviour
+{
+    [SerializeField] EnemyHealth healthBar;
+
+    [SerializeField] float maxHealth = 100;
+    [SerializeField] float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<EnemyHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        if (health <= 0)
+        {
+            print("ded");
+        }
+    }
+
+    // The Toiler flashbacks (BUT BETTER BECAUSE THE DAMAGE IS CONTAINED WITHIN THE ENEMY SCRIPT, NOT THE HITBOX OF THE BULLET WOOOOOOO)
+    private void OnTriggerEnter(Collider bullet)
+    {
+        if (bullet.name == "TempBullet(Clone)")
+        {
+           TakeDamage(20);
+        }
+        
+    }
+}
