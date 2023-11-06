16183fb0ecbf9ca7729efd5d3a2e9583e1678eae
i8yopancake <RaymondPalacio711@yahoo.com>
2023-11-05 18:59:16 -0800

    syntax
    fixed some syntax

--- Assets/Scripts/AI/SerpentAI.cs
+++ Assets/Scripts/AI/SerpentAI.cs
@@ -11,6 +11,8 @@ public class SerpentAI : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
+    public float health;
+
     //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
@@ -19,6 +21,7 @@ public class SerpentAI : MonoBehaviour
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
+    public GameObject projectile;
 
     //States
     public float sightRange, attackRange;
@@ -34,12 +37,12 @@ public class SerpentAI : MonoBehaviour
     private void Update()
     {
         //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position,sightRange,whatIsPlayer)
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer)
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer(); //Switch with patroling, need to burrow when player in range. attack when player out of range.
-        if (playerinAttackRange && playerInSightRange) AttackPlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
 
     private void Patroling()
@@ -62,7 +65,7 @@ public class SerpentAI : MonoBehaviour
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ)
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             walkPointSet = true;
@@ -82,12 +85,40 @@ public class SerpentAI : MonoBehaviour
 
         if (!alreadyAttacked)
         {
-            //Attack 
+            //Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
 
         }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0) 
+            Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void DestroyEnemy(GameObject gameObject)
+    {
+        DestroyEnemy(gameObject);
     }
 
     // Start is called before the first frame update
