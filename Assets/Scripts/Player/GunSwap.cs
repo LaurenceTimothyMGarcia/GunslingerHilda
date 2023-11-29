d4232219903453054ad6cd8e9cf4415cc0f4e4ad
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-11-29 11:26:17 -0800

    started gun swap

---
+++ Assets/Scripts/Player/GunSwap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSwap : MonoBehaviour
+{
+    public GameObject revolver;
+    public GameObject shotgun;
+    public GameObject sniper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
